bb521a12447c4394b5e83ee5ac93800cfbaf9c04
jirivchi1 <jiriverachinchuna@al.uloyola.es>
2024-11-12 20:15:45 +0100

    add port and baud rate in config.py

--- config.py
+++ config.py
@@ -16,3 +16,10 @@ SENSOR_DATA_FILE = os.getenv(
 MONITORING_URL = os.getenv("MONITORING_URL")
 DEVICE_ID = os.getenv("DEVICE_ID")
 API_PASSWORD = os.getenv("API_PASSWORD")
+
+
+# Configuración del puerto serial y reintentos de conexión con Arduino
+port = (
+    "/dev/serial0"  # Asegúrate de que este puerto coincida con el puerto de tu Arduino
+)
+baud_rate = 9600
