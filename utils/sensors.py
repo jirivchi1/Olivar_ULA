27fc7da80804ff403f81fcd75f04ad2fe3c38cc6
jirivchi1 <jiriverachinchuna@al.uloyola.es>
2024-11-14 10:16:38 +0100

    add log to temp and humid sensor

--- utils/sensors.py
+++ utils/sensors.py
@@ -1,14 +1,18 @@
 from sht20 import SHT20
 
+# Registra el error y devuelve valores predeterminados
+from utils.logger import log_action
+
+
 def read_sensor_data():
     try:
         sht = SHT20(1, resolution=SHT20.TEMP_RES_14bit)
         data = sht.read_all()
         temp = round(data[0], 2)
         humid = round(data[1], 2)
+        log_action(f"Datos sensor temp: {temp}, hum: {humid}")
         return temp, humid
+
     except Exception as e:
-        # Registra el error y devuelve valores predeterminados
-        from utils.logger import log_action
         log_action(f"Error al leer los datos del sensor: {e}")
         return None, None  # Devuelve valores nulos o predeterminados
