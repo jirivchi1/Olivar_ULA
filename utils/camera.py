8c708e083720cc531af82b15e59425faa5823494
Ismael Rivera <117596961+jirivchi1@users.noreply.github.com>
2024-11-19 12:50:02 +0100

    Update camera.py

--- utils/camera.py
+++ utils/camera.py
@@ -3,11 +3,25 @@ import subprocess
 from datetime import datetime
 from utils.logger import log_action
 
-
 def take_photo(local_directory, archivo, name_foto):
     os.makedirs(local_directory, exist_ok=True)
     filename = datetime.now().strftime("%Y%m%d_%H%M%S") + name_foto + ".jpg"
-    filepath = f"{local_directory}/{filename}"
-    subprocess.run(["fswebcam", "-r", "1280x720", "--no-banner", filepath], check=True)
-    log_action(f"Photo {filename} taken.", archivo)
-    return filepath, filename
+    filepath = os.path.join(local_directory, filename)
+    
+    try:
+        # Intentar tomar la foto
+        subprocess.run(["fswebcam", "-r", "1280x720", "--no-banner", filepath], check=True)
+        
+        # Verificar si el archivo se creó
+        if os.path.exists(filepath):
+            log_action(f"Photo {filename} successfully taken and saved to {filepath}.", archivo)
+            return filepath, filename
+        else:
+            log_action(f"Photo {filename} could not be saved despite no errors.", archivo)
+            return None, None
+    except subprocess.CalledProcessError as e:
+        log_action(f"Failed to take photo {filename}. Error: {str(e)}", archivo)
+        return None, None
+    except Exception as e:
+        log_action(f"Unexpected error while taking photo {filename}. Error: {str(e)}", archivo)
+        return None, None
