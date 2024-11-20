84d7d0c85d984ab934115cb398cc13aefeaf5a7f
Ismael Rivera <117596961+jirivchi1@users.noreply.github.com>
2024-11-20 13:41:33 +0100

    Update logger.py update to get user for metrics_directory

--- utils/logger.py
+++ utils/logger.py
@@ -1,8 +1,13 @@
 import os
 from datetime import datetime
-from config import METRICS_DIRECTORY
 
+# Obtener el directorio base del usuario actual dinámicamente
+METRICS_DIRECTORY = os.path.join(os.path.expanduser("~"), "metrics")
 
-def log_action(message, arhivo):
-    with open(f"{METRICS_DIRECTORY}/{arhivo}", "a") as log_file:
+def log_action(message, archivo):
+    # Asegurar que el directorio de métricas exista
+    os.makedirs(METRICS_DIRECTORY, exist_ok=True)
+    
+    # Crear o abrir el archivo de log y escribir el mensaje
+    with open(f"{METRICS_DIRECTORY}/{archivo}", "a") as log_file:
         log_file.write(f"{datetime.now()}: {message}\n")
