751fa62aeeef5c4383933ec5dbb5430697e35fe4
Ismael Rivera <117596961+jirivchi1@users.noreply.github.com>
2024-11-21 12:50:19 +0100

    Update yellow_main.py

--- yellow_main.py
+++ yellow_main.py
@@ -4,8 +4,12 @@ from utils.server_upload import upload_to_server, delete_photos
 from utils.logger import log_action
 from utils.monitoring import send_monitoring_data_yellow
 from utils.shutdown import shutdown_system
-from config import LOCAL_DIRECTORY_YELLOW, SERVER_DIR_YELLOW
+from config import SERVER_DIR_YELLOW
+import os
 
+# Obtener el directorio base dinámicamente
+LOCAL_DIRECTORY_YELLOW = os.path.join(os.path.expanduser("~"), "olivarv2", "fotos_yellow")
+os.makedirs(LOCAL_DIRECTORY_YELLOW, exist_ok=True)
 
 def main():
     try:
