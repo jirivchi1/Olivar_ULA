12b10a67cbd69cc7cb30ce8a3a55ac2f634a897e
Ismael Rivera <117596961+jirivchi1@users.noreply.github.com>
2025-02-17 11:10:44 +0100

    Update camera.py change command to take picture with raspberry pi

--- utils/camera.py
+++ utils/camera.py
@@ -18,7 +18,7 @@ def take_photo(local_directory, archivo, name_foto):
 
     try:
         # Intentar tomar la foto
-        subprocess.run(["fswebcam", "-r", "1280x720", "--no-banner", filepath], check=True)
+        subprocess.run(["libcamera-jpeg", "-o", filepath, "--width","3280","--height","2464"], check=True)
         
         # Verificar si el archivo se creó
         if os.path.exists(filepath):
