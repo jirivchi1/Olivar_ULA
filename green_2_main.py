76d217e6701d3e5592d7b70ee796a596e3c67092
Ismael Rivera <117596961+jirivchi1@users.noreply.github.com>
2025-02-18 12:57:08 +0100

    Update green_2_main.py

--- green_2_main.py
+++ green_2_main.py
@@ -4,7 +4,7 @@ from utils.server_upload import upload_to_server, delete_photos
 from utils.logger import log_action
 from config import SERVER_DIR_GREEN_2
 
-from utils.monitoring import send_monitoring_data_green
+from utils.monitoring import send_monitoring_data_green_2
 from utils.shutdown import shutdown_system
 
 import os
